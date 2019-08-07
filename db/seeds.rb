b75a29c501f953835da38ca9165245aab36a8c51
Luix Roussarie <luixitos@hotmail.com>
2019-08-07 16:58:02 +0200

    railsjob

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+category_choice = %w(chinese italian japanese french belgian)
+
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '0618225584',
+    category:       category_choice.shuffle.first,
+
+  },
+  {
+    name:         'Jiji',
+    address:      'Carotte, Birmingham E2 7JE',
+    phone_number:  '0618349876',
+    category:       category_choice.shuffle.first,
+  },
+  {
+    name:         'Toto',
+    address:      'Jp, Oxford E2 33E',
+    phone_number:  '0633878233',
+    category:      category_choice.shuffle.first,
+  },
+  {
+    name:         'Vava',
+    address:      'Peyrehorade, 40300',
+    phone_number:  '0623873233',
+    category:      category_choice.shuffle.first,
+  },
+  {
+    name:         'Lolo',
+    address:      'Ferrari, 87330',
+    phone_number:  '0633893234',
+    category:      category_choice.shuffle.first,
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
